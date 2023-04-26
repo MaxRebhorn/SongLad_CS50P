b8ee91db534d72785f2f948d71828264347291de
MaxRebhorn <rebhorn.max@outlook.de>
2023-04-26 14:20:01 +0200

    worked on the find song function a bit now works kind of not tested yet though

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import praw
 import prawcore.exceptions
+import requests
+import re
+import lyricsgenius as genius
 
 def main():
     global reddit_api_counter
     reddit_api_counter = 0
     Bot = create_reddit_bot()
-    search_for_mention(Bot)
+    songname,mention = search_for_mention(Bot)
+    song_lyrics = find_song(songname,mention)
+    mention.reply(song_lyrics)
 
 
 def create_reddit_bot():
@@ -26,16 +31,27 @@ def search_for_mention(Bot):
                 comments += 1
                 print(comments)
                 if isinstance(mention,praw.models.Comment):
-                    if mention not in already_replied_to_comment:
-                        if mention.submission.is_video:
-                            print("Found one")
-                            already_replied_to_comment.add(mention)
-                            mention.reply("Hello i am bot")
+                    if mention.new == True:
+                        print("Found one")
+                        already_replied_to_comment.add(mention)
+                        mention.mark_read()
+                        mention.reply("Hello i am bot")
+                        try:
+                            songname = re.sub(r"u/SongLad", "", mention.body, flags=re.IGNORECASE)
+                        except ValueError:
+                            mention.reply("no song title was given")
 
         except prawcore.exceptions.BadRequest:
             print("error")
-def find_song():
-    ...
+    return songname,mention
+def find_song(songname):
+    Genius = genius.Genius("zH_HyGzT82X43jyLRuf4q2NX1rRp63rPy2sNU4wXNf5nlcmHTgrjhzPwVS19UkQw")
+    song = Genius.search_song(songname)
+    if song != None:
+        return song.lyrics
+    else:
+        return None
+
 #test if git works lel aaaah waaaaaaaa
 
 if __name__ == "__main__":
